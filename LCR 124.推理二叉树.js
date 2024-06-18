aa228b552847d38818a5bca27cd44b5f6dffd29c
ragnarkor <466023842@qq.com>
2024-06-18 09:51:49 +0800

    重建二叉树

--- LCR 124.推理二叉树.js
+++ LCR 124.推理二叉树.js
@@ -24,11 +24,43 @@
  * @return {TreeNode}
  */
 var deduceTree = function(preorder, inorder) {
-
-};
+    const inOrderHash = new Map()
+    inorder.forEach((val, index) => inOrderHash.set(val, index))
+    
+    /**
+     * 重建二叉树
+     * @param {*} preLeft 前序数组左边界
+     * @param {*} preRight 前序数组右边界
+     * @param {*} inLeft 中序数组左边界
+     * @param {*} inRight 中序数组右边界
+     * @returns 
+     */
+    function buildTree(preLeft, preRight, inLeft, inRight){
+      if(preLeft > preRight || inLeft > inRight)  // 当没有子节点的时候
+        return null
+  
+      let preRoot = preLeft;  // 前序遍历的根节点就是左边界
+      let inRoot = inOrderHash.get(preorder[preRoot])  // 寻找中序遍历的根节点位置
+      const node = new TreeNode(preorder[preRoot]) // 创建根节点
+  
+      let leftSize = inRoot - inLeft // 得到左子树节点数目
+      // 画图理解索引计算关系
+      node.left = buildTree(preRoot+1, preRoot+leftSize, inLeft, inRoot-1)
+      node.right = buildTree(preRoot+leftSize+1, preRight, inRoot+1, inRight)
+      return node
+    }
+  
+    return buildTree(0, preorder.length-1, 0, inorder.length-1)
+  };
 // @lc code=end
 
-
+/**
+ * 前序遍历为[根|左|右]
+ * 中序遍历为[左|根|右]
+ * 思路：
+ * 1. 通过前序遍历确定根节点
+ * 2. 在中序遍历中找到根节点索引，划分左右子树，完成重建
+ */
 
 /*
 // @lcpr case=start
