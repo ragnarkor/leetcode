dec28cad3f381964ca86349a358912587cc7c0aa
ragnarkor <466023842@qq.com>
2024-08-13 10:10:28 +0800

    位运算·快速幂

---
+++ LCR 134.pow-x-n.js
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=LCR 134 lang=javascript
+ * @lcpr version=30204
+ *
+ * [LCR 134] Pow(x, n)
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    if(x === 0) return 0    // 0的幂为0
+    // 当n为负数时
+    if(n < 0){
+        x = 1 / x
+        n--
+    }
+    
+    let res = 1
+    while(n){
+        if(n & 1)   // 从右往左取指数
+            res *= x
+        x *= x  //  底数平方
+        n /= 2 // 右移一位(不知道为什么用n>>>1会溢出)
+    }
+    return res
+};
+// @lc code=end
+
+/**
+ * 题解：Goodnotes-位运算·数值的整数次方
+ */
+
+/*
+// @lcpr case=start
+// 2.00000\n10\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 2.10000\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 2.00000\n-2\n
+// @lcpr case=end
+
+ */
+
